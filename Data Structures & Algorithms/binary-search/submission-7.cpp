2ebee5219a9e27e5bce2ab7790653e91b4fed2cc
Ximena <70961966+x4nemi@users.noreply.github.com>
2026-08-11 14:29:48 -0600

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size() == 1 and nums[0] != target) return -1;
+        int L = 0, R = nums.size();
+
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            cout << L << " " << mid << " " << R << endl; 
+            if (target > nums[mid]) {
+                L = mid + 1;
+            } else if (target < nums[mid]) {
+                R = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+        
+    }
+};
