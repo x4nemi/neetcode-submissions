36c8a2be7c2d416ccb98c29b5b80e7fa258239bb
Ximena <70961966+x4nemi@users.noreply.github.com>
2026-08-17 16:45:23 -0600

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const map = new Map<number,number>();
+        for(let i = 0; i < nums.length; i++){
+            if(map.has(nums[i])) return true;
+            map.set(nums[i], 1);
+        }
+
+        return false;
+    }
+}
