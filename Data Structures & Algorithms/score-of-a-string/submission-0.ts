089cb244581d62112edd4078f66799d75c7b0f22
Ximena <70961966+x4nemi@users.noreply.github.com>
2026-08-17 16:57:35 -0600

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    scoreOfString(s: string): number {
+        let result = 0;
+
+        for(let i = 1; i < s.length; i++) {
+            result += Math.abs(s.charCodeAt(i) - s.charCodeAt(i-1))
+            // console.log(s.charCodeAt(i), s.charCodeAt(i+1))
+        }
+        return result
+    }
+}
